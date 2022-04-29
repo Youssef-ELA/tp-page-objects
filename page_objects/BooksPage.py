97c0b2a74ce04f12b38f124fb82d91d305049b5d
Y.ELACHHAB <elachhabyoussef@gmail.com>
2022-04-29 16:24:04 +0200

    changement au niveau de bookPage

--- page_objects/BooksPage.py
+++ page_objects/BooksPage.py
@@ -4,10 +4,12 @@ from selenium.webdriver.common.by import By
 
 
 class BooksPage:
+    premier_article_nouveaute = 'div.a-section.octopus-pc-card-content li '
 
     def __init__(self, driver: webdriver.Chrome):
         self.driver = driver
-    premier_article_nouveaute = 'div.a-section.octopus-pc-card-content li '
+
+
     def selectFirstBookNouveautes(self):
         self.driver.find_element(By.CSS_SELECTOR, self.premier_article_nouveaute).click()
 

db4d610230c10e6b6e22fbe1dd2e052cd5e17dbc
Y.ELACHHAB <elachhabyoussef@gmail.com>
2022-04-29 16:20:03 +0200

    changement au niveau de bookPage, suppression de deux lignes

--- page_objects/BooksPage.py
+++ page_objects/BooksPage.py
@@ -7,9 +7,7 @@ class BooksPage:
 
     def __init__(self, driver: webdriver.Chrome):
         self.driver = driver
-
     premier_article_nouveaute = 'div.a-section.octopus-pc-card-content li '
-
     def selectFirstBookNouveautes(self):
         self.driver.find_element(By.CSS_SELECTOR, self.premier_article_nouveaute).click()
 
